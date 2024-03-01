301f95d791031ed1d63fe573093959d83f79b60b
sachin-rohilla <sachinrohilla23799@gmail.com>
2024-03-01 17:01:01 +0530

    add-react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,6 @@ function App() {
   };
 
   const handleNext = () => {
-    console.log("hello");
     if (
       (currentStep === 1 && formData.firstName && formData.lastName) ||
       (currentStep === 2 && formData.email && formData.confirmEmail) ||
@@ -61,119 +60,121 @@ function App() {
   console.log(formData, currentStep);
   return (
     <>
-      <div className="w-96 mx-auto">
-        <h1 className="font-bold text-xl text-center">Multi Step Form</h1>
-        <form
-          className="flex flex-col items-center gap-2"
-          onSubmit={handleSubmit}
-        >
-          {currentStep === 1 && (
-            <>
-              <InputComp
-                title="firstName"
-                type="text"
-                placeholder="enter your first name"
-                value={formData?.firstName}
-                handleChange={handleChange}
-              />
-              <InputComp
-                title="lastName"
-                type="text"
-                placeholder="enter your last name"
-                value={formData?.lastName}
-                handleChange={handleChange}
-              />
-            </>
-          )}
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="w-96 h-96 m-auto border p-4 shadow-lg rounded-lg">
+          <h1 className="font-bold text-xl text-center">Multi Step Form</h1>
+          <form
+            className="flex flex-col  items-center gap-2"
+            onSubmit={handleSubmit}
+          >
+            {currentStep === 1 && (
+              <>
+                <InputComp
+                  title="firstName"
+                  type="text"
+                  placeholder="Enter your first name"
+                  value={formData?.firstName}
+                  handleChange={handleChange}
+                />
+                <InputComp
+                  title="lastName"
+                  type="text"
+                  placeholder="Enter your last name"
+                  value={formData?.lastName}
+                  handleChange={handleChange}
+                />
+              </>
+            )}
 
-          {currentStep === 2 && (
-            <>
-              <InputComp
-                title="email"
-                type="email"
-                placeholder="enter your email"
-                value={formData?.email}
-                handleChange={handleChange}
-              />
-              <InputComp
-                title="confirmEmail"
-                type="email"
-                placeholder="enter your confirm email"
-                value={formData?.confirmEmail}
-                handleChange={handleChange}
-              />
-            </>
-          )}
+            {currentStep === 2 && (
+              <>
+                <InputComp
+                  title="email"
+                  type="email"
+                  placeholder="enter your email"
+                  value={formData?.email}
+                  handleChange={handleChange}
+                />
+                <InputComp
+                  title="confirmEmail"
+                  type="email"
+                  placeholder="enter your confirm email"
+                  value={formData?.confirmEmail}
+                  handleChange={handleChange}
+                />
+              </>
+            )}
 
-          {currentStep === 3 && (
-            <>
-              <InputComp
-                title="password"
-                type="password"
-                placeholder="enter your password"
-                value={formData?.password}
-                handleChange={handleChange}
-              />
-              <InputComp
-                title="confirmPassword"
-                type="password"
-                placeholder="enter your confirm password"
-                value={formData?.confirmPassword}
-                handleChange={handleChange}
-              />
-            </>
-          )}
+            {currentStep === 3 && (
+              <>
+                <InputComp
+                  title="password"
+                  type="password"
+                  placeholder="enter your password"
+                  value={formData?.password}
+                  handleChange={handleChange}
+                />
+                <InputComp
+                  title="confirmPassword"
+                  type="password"
+                  placeholder="enter your confirm password"
+                  value={formData?.confirmPassword}
+                  handleChange={handleChange}
+                />
+              </>
+            )}
 
-          {currentStep === 4 && (
-            <>
-              <InputComp
-                title="gender"
-                type="text"
-                placeholder="enter your gender"
-                value={formData?.gender}
-                handleChange={handleChange}
-              />
-              <InputComp
-                title="role"
-                type="text"
-                placeholder="enter your role"
-                value={formData?.role}
-                handleChange={handleChange}
-              />
-            </>
-          )}
+            {currentStep === 4 && (
+              <>
+                <InputComp
+                  title="gender"
+                  type="text"
+                  placeholder="enter your gender"
+                  value={formData?.gender}
+                  handleChange={handleChange}
+                />
+                <InputComp
+                  title="role"
+                  type="text"
+                  placeholder="enter your role"
+                  value={formData?.role}
+                  handleChange={handleChange}
+                />
+              </>
+            )}
 
-          <div className="w-full flex justify-between">
-            <button
-              className={`px-4 py-2 bg-blue-400 rounded-md text-white ${
-                currentStep === 1
-                  ? "opacity-45 cursor-not-allowed"
-                  : "opacity-100"
-              }`}
-              type="button"
-              onClick={handlePrev}
-              disabled={currentStep === 1}
-            >
-              Prev
-            </button>
-            <button
-              className="px-4 py-2 bg-blue-400 rounded-md text-white"
-              type={currentStep === 4 ? "submit" : "button"}
-              onClick={handleNext}
-              disabled={
-                (currentStep === 1 &&
-                  !(formData.firstName && formData.lastName)) ||
-                (currentStep === 2 &&
-                  !(formData.email && formData.confirmEmail)) ||
-                (currentStep === 3 &&
-                  !(formData.password && formData.confirmPassword)) ||
-                (currentStep === 4 && !(formData.gender && formData.role))
-              }
-            >
-              {currentStep === 4 ? "Submit" : "Next"}
-            </button>
-          </div>
-        </form>
+            <div className="w-full flex justify-between">
+              <button
+                className={`px-4 py-2 bg-blue-400 rounded-md text-white ${
+                  currentStep === 1
+                    ? "opacity-45 cursor-not-allowed"
+                    : "opacity-100"
+                }`}
+                type="button"
+                onClick={handlePrev}
+                disabled={currentStep === 1}
+              >
+                Prev
+              </button>
+              <button
+                className="px-4 py-2 bg-blue-400 rounded-md text-white"
+                type={currentStep === 4 ? "submit" : "button"}
+                onClick={handleNext}
+                disabled={
+                  (currentStep === 1 &&
+                    !(formData.firstName && formData.lastName)) ||
+                  (currentStep === 2 &&
+                    !(formData.email && formData.confirmEmail)) ||
+                  (currentStep === 3 &&
+                    !(formData.password && formData.confirmPassword)) ||
+                  (currentStep === 4 && !(formData.gender && formData.role))
+                }
+              >
+                {currentStep === 4 ? "Submit" : "Next"}
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
